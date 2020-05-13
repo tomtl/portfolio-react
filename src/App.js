7f43f781f96638dd362f68c168044d3bdf2d1836
Tom Lee <tomtleee@gmail.com>
2020-05-13 08:23:39 -0400

    Create home component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import { Home } from './contents/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export class App extends React.Component {
+    render() {
+        return <Home />
+    }
+};
