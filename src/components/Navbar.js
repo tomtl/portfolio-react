101360e7bd09859dbadb5e84d22bb5be04e10ad8
Tom Lee <tomtleee@gmail.com>
2020-05-13 08:33:03 -0400

    Setup Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class Navbar extends React.Component {
+    render() {
+        return(
+            <nav class="navbar navbar-expand-lg">
+                <a class="navbar-brand" href="index.html">TOM LEE</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-item nav-link active" href="index.html">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-item nav-link" href="portfolio.html">Portfolio</a>
+                        <a class="nav-item nav-link" href="resume.html">Resume</a>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+};
--- src/contents/Home.js
+++ src/contents/Home.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Navbar } from '../components/Navbar'
 
 export class Home extends React.Component {
     render() {
         return (
           <div className="App">
 
-              <nav class="navbar navbar-expand-lg">
-                <a class="navbar-brand" href="index.html">TOM LEE</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                  <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="index.html">Home <span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link" href="portfolio.html">Portfolio</a>
-                    <a class="nav-item nav-link" href="resume.html">Resume</a>
-                  </div>
-                </div>
-              </nav>
+            <Navbar />
 
               <div class="jumbotron jumbotron-fluid">
                 <div class="container">
