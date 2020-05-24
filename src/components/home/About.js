a42bfbaf8b7d4ea7938d8ab51e6c305ffb80ee2e
Tom Lee <tomtleee@gmail.com>
2020-05-24 06:54:23 -0400

    Refactor image mouseover events

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -1,16 +1,31 @@
 import React from 'react';
 
 export class About extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { imageSrc: "images/mugshot.png" };
+    this.showAlternateMugshot = this.showAlternateMugshot.bind(this);
+    this.showNormalMugshot = this.showNormalMugshot.bind(this);
+  }
+
+  showAlternateMugshot() {
+    this.setState({ imageSrc: "images/mugshot_v2.png" });
+  }
+
+  showNormalMugshot() {
+    this.setState({ imageSrc: "images/mugshot.png" });
+  }
+
   render() {
     return(
       <section class="content" id="about">
         <h1>About me</h1>
         <div class="row align-items-center">
           <div class="col-md-3">
-            <img src='images/mugshot.png'
+            <img src={this.state.imageSrc}
               alt="mugshot"
-              onMouseOver={e => (e.currentTarget.src = "images/mugshot_v2.png")}
-              onMouseOut={e => (e.currentTarget.src = "images/mugshot.png")} />
+              onMouseOver={this.showAlternateMugshot}
+              onMouseOut={this.showNormalMugshot} />
           </div>
           <div class="col-md-9">
             <p class="lead">I’m a GIS analyst specializing in geospatial data analysis, GIS development, and interactive cartographic design. I live in Boston, MA and am originally from Perth, Western Australia. I get excited about coding data models, interrogating spatial relationships, and designing cartographic products that bring insight to complex problems.</p>
@@ -20,3 +35,5 @@ export class About extends React.Component {
     )
   }
 };
+
+// onMouseOver={e => (e.currentTarget.src = "images/mugshot_v2.png")}
