715043dd4a204c96307bb2d0d0d2c40f03d974d0
Tom Lee <tomtleee@gmail.com>
2020-05-26 12:27:21 -0400

    Updates

---
+++ precache-manifest.8ccd73c9febcbf6dc478a4a817d89cba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a556e8900d21a3e3c180a65d3ea918b",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "b174bcb61568df864773",
+    "url": "/portfolio-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c48e32950c2324a1d0b6",
+    "url": "/portfolio-react/static/css/main.45eab854.chunk.css"
+  },
+  {
+    "revision": "b174bcb61568df864773",
+    "url": "/portfolio-react/static/js/2.9b58e38c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-react/static/js/2.9b58e38c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c48e32950c2324a1d0b6",
+    "url": "/portfolio-react/static/js/main.a2ee79f4.chunk.js"
+  },
+  {
+    "revision": "cd7dd7ad56d43584e1af",
+    "url": "/portfolio-react/static/js/runtime-main.0e07d887.js"
+  },
+  {
+    "revision": "af22d28d662a603e2f5b8c093b942bae",
+    "url": "/portfolio-react/static/media/water.af22d28d.png"
+  }
+]);
